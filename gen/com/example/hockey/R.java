d74b98dd1e7f39f67227dfd6831f5935616e2a42
satellitex <s.a.t.e.3.ths@gmail.com>
2014-03-24 20:43:17 +0900

    ver1.0(広告有)

--- gen/com/example/hockey/R.java
+++ gen/com/example/hockey/R.java
@@ -17,8 +17,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int asobikatabuttonoff=0x7f020000;
@@ -74,25 +74,26 @@ public final class R {
         public static final int winner=0x7f020032;
     }
     public static final class id {
-        public static final int action_settings=0x7f080012;
+        public static final int action_settings=0x7f080013;
+        public static final int adView=0x7f080002;
         public static final int button01=0x7f080000;
         public static final int button02=0x7f080001;
-        public static final int imagebutton1=0x7f080004;
-        public static final int imagebutton2=0x7f080005;
-        public static final int imagebutton3=0x7f080006;
-        public static final int losecount=0x7f08000f;
-        public static final int nonpaireddevicelist=0x7f080008;
-        public static final int pager=0x7f080009;
-        public static final int paireddevicelist=0x7f080007;
-        public static final int resulttext=0x7f080010;
-        public static final int returnbutton=0x7f080011;
-        public static final int servername=0x7f080002;
-        public static final int textlose=0x7f08000e;
-        public static final int textresult=0x7f08000b;
-        public static final int textview1=0x7f080003;
-        public static final int textwin=0x7f08000c;
-        public static final int twitterbutton=0x7f08000a;
-        public static final int wincount=0x7f08000d;
+        public static final int imagebutton1=0x7f080005;
+        public static final int imagebutton2=0x7f080006;
+        public static final int imagebutton3=0x7f080007;
+        public static final int losecount=0x7f080010;
+        public static final int nonpaireddevicelist=0x7f080009;
+        public static final int pager=0x7f08000a;
+        public static final int paireddevicelist=0x7f080008;
+        public static final int resulttext=0x7f080011;
+        public static final int returnbutton=0x7f080012;
+        public static final int servername=0x7f080003;
+        public static final int textlose=0x7f08000f;
+        public static final int textresult=0x7f08000c;
+        public static final int textview1=0x7f080004;
+        public static final int textwin=0x7f08000d;
+        public static final int twitterbutton=0x7f08000b;
+        public static final int wincount=0x7f08000e;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -124,9 +125,10 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f040002;
+        public static final int admob_unit_id=0x7f040000;
+        public static final int app_name=0x7f040001;
+        public static final int hello_world=0x7f040003;
     }
     public static final class style {
         /** 
